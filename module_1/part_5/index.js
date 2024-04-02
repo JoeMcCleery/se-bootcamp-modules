8c503034f9679f49da2f8b25d2c4f682caad9429
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-02 21:12:16 +0800

    m1p5

---
+++ module_1/part_5/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const Sentiment = require("sentiment");
+
+const app = express();
+const sentiment = new Sentiment();
+
+app.get("/", (req, res) => {
+  res.send(getSentiment("Coding is great, but javascript is meh!"));
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+function getSentiment(sentence) {
+  return sentiment.analyze(sentence);
+}
