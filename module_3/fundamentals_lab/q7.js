b6fd47a2eff0533779a5b0600bae2151ff019e15
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-23 19:13:39 +0800

    js fundamentals q7

---
+++ module_3/fundamentals_lab/q7.js
@@ -0,0 +1,59 @@
+// The following object represents a basketball game and keeps track of the score as the
+// game progresses.
+// a) Modify each of the methods so that they can be ‘chained’ together and the last line of
+// the example code works
+// b) Add a new method to print the full time final score
+// c) Add a new object property to keep track of the number of fouls and a method to
+// increment it, similar but separate to the score. Include the foul count in the half time and
+// full time console messages
+// d) Test your object by chaining all the method calls together in different combinations.
+
+const basketballGame = {
+  score: 0,
+  fouls: 0,
+  // Score Methods
+  freeThrow() {
+    this.score++;
+    return this;
+  },
+  basket() {
+    this.score += 2;
+    return this;
+  },
+  threePointer() {
+    this.score += 3;
+    return this;
+  },
+  // Foul methods
+  foul() {
+    this.fouls++;
+    return this;
+  },
+  // Summary methods
+  halfTime() {
+    console.log(
+      `Halftime score is ${this.score}. There were ${this.fouls} fouls.`
+    );
+    return this;
+  },
+  fullTime() {
+    console.log(
+      `Fulltime score is ${this.score}. There were ${this.fouls} fouls.`
+    );
+    return this;
+  },
+};
+//modify each of the above object methods to enable function chaining as below:
+basketballGame
+  .basket()
+  .freeThrow()
+  .freeThrow()
+  .basket()
+  .threePointer()
+  .halfTime()
+  .basket()
+  .threePointer()
+  .foul()
+  .foul()
+  .basket()
+  .fullTime();
