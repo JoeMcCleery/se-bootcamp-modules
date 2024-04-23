a034b44bdaa956a5d4ff2481e4dcfb7b0080972b
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-23 18:58:24 +0800

    js fundamentals q5

---
+++ module_3/fundamentals_lab/q5.js
@@ -0,0 +1,17 @@
+// Rewrite the following function using: a) function expression syntax, and b) arrow function
+// syntax. Test each version to make sure they work the same.
+
+function getGreeting(name) {
+  return "Hello " + name + "!";
+}
+
+// Answer
+// a
+const getGreetings_function = function (name) {
+  return "Hello " + name + "!";
+};
+
+// b
+const getGreetings_arrow = (name) => {
+  return "Hello " + name + "!";
+};
