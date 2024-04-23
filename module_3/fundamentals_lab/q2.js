db6fc0569b636b7d159b0373e76ce3c4aa35fd19
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-23 18:49:17 +0800

    js fundamentals q2

---
+++ module_3/fundamentals_lab/q2.js
@@ -0,0 +1,17 @@
+let three = "3";
+let four = "4";
+let thirty = "30";
+//what is the value of the following expressions?
+let addition = three + four;
+let multiplication = three * four;
+let division = three / four;
+let subtraction = three - four;
+let lessThan1 = three < four;
+let lessThan2 = thirty < four;
+
+console.log(addition); // "34"
+console.log(multiplication); // 12
+console.log(division); // 0.75
+console.log(subtraction); // -1
+console.log(lessThan1); // true
+console.log(lessThan2); // true
