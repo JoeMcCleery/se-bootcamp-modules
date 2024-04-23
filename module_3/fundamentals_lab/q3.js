b576ae50e68ce8768d3c897cd37d67282306080e
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-23 18:52:19 +0800

    js fundamentals q3

---
+++ module_3/fundamentals_lab/q3.js
@@ -0,0 +1,5 @@
+if (0) console.log("#1 zero is true");
+if ("0") console.log("#2 zero is true"); // non-empty string is truthy
+if (null) console.log("null is true");
+if (-1) console.log("negative is true"); // non-0 number is truthy
+if (1) console.log("positive is true"); // non-0 number is truthy
