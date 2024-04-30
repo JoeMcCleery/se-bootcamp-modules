fd8db7f2016cc3ab1d362f9791e1a37761046916
JoeMcCleery <mccleery.joseph@gmail.com>
2024-04-30 20:21:42 +0800

    lab5 messing around

---
+++ module_3/lab_5/messingAround.js
@@ -0,0 +1,48 @@
+function Student() {
+  this.name = "John Doe";
+  this.age = 20;
+  this.course = new Course();
+}
+
+function Course() {
+  this.name = "ICT";
+  this.campus = "Perth";
+  this.grade = new Grade();
+}
+
+function Grade() {
+  this.grade = 0;
+  this.getLetterGrade = () => {
+    if (this.grade >= 85) {
+      return "HD";
+    } else if (this.grade > 75) {
+      return "D";
+    } else if (this.grade > 65) {
+      return "C";
+    } else if (this.grade > 50) {
+      return "P";
+    } else {
+      return "F";
+    }
+  };
+}
+
+const student1 = new Student();
+const student2 = new Student();
+
+console.group("Student Profiles");
+console.log("student1", student1);
+console.log(`student2 ${student2}`);
+console.groupEnd();
+
+function testFunc() {
+  if (true) {
+    var varTest = "var";
+    let letTest = "let";
+  }
+
+  console.log(varTest); // "var"
+  console.log(letTest); // ReferenceError: letTest is not defined
+}
+
+testFunc();
