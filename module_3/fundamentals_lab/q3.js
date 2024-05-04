03a1d6ecc64e08c6dabfa992ffd4b009ac1fdd0c
JoeMcCleery <mccleery.joseph@gmail.com>
2024-05-04 11:56:44 +0800

    fundamentals_lab q3

--- module_3/fundamentals_lab/q3.js
+++ module_3/fundamentals_lab/q3.js
@@ -1,7 +1,7 @@
 // 3. Which of the following console.log messages will print? Why?
 
-if (0) console.log("#1 zero is true");
+if (0) console.log("#1 zero is true"); // 0 is falsey
 if ("0") console.log("#2 zero is true"); // non-empty string is truthy
-if (null) console.log("null is true");
+if (null) console.log("null is true"); // null is falsey
 if (-1) console.log("negative is true"); // non-0 number is truthy
 if (1) console.log("positive is true"); // non-0 number is truthy
