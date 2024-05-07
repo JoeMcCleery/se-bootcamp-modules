c0c4b41b2e41416d49b7a9c186b89624c4dd8249
JoeMcCleery <mccleery.joseph@gmail.com>
2024-05-07 20:50:16 +0800

    advanced_lab q1

---
+++ module_3/advanced_lab/q1.js
@@ -0,0 +1,22 @@
+// 1. makeCounter below is a decorator function which creates and returns a function that
+// increments a counter.
+// a) Create a second counter counter2 using the makeCounter function and test to see if
+// it remains independent to counter1
+// b) Modify makeCounter so that it takes an argument startFrom specifying where the
+// counter starts from (instead of always starting from 0)
+// c) Modify makeCounter to take another argument incrementBy, which specifies
+function makeCounter(startFrom = 0, incrementBy = 1) {
+  let currentCount = startFrom;
+  return function () {
+    currentCount += incrementBy;
+    console.log(currentCount);
+    return currentCount;
+  };
+}
+let counter1 = makeCounter();
+counter1(); // 1
+counter1(); // 2
+
+// a)
+let counter2 = makeCounter(10, 10);
+counter2(); // 20
