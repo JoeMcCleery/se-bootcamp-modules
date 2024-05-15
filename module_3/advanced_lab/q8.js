94e138606e6530377bfd8d8df8b9fcad4eddae13
JoeMcCleery <mccleery.joseph@gmail.com>
2024-05-15 14:36:13 +0800

    advanced_lab q8

---
+++ module_3/advanced_lab/q8.js
@@ -0,0 +1,84 @@
+// 8. The following DigitalClock class uses an interval to print the time every second once
+// started, until stopped.
+class DigitalClock {
+  constructor(prefix) {
+    this.prefix = prefix;
+  }
+  display() {
+    let date = new Date();
+    //create 3 variables in one go using array destructuring
+    let [hours, mins, secs] = [
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+    ];
+    if (hours < 10) hours = "0" + hours;
+    if (mins < 10) mins = "0" + mins;
+    if (secs < 10) secs = "0" + secs;
+    console.log(`${this.prefix} ${hours}:${mins}:${secs}`);
+  }
+  stop() {
+    clearInterval(this.timer);
+  }
+  start() {
+    this.display();
+    this.timer = setInterval(() => this.display(), 1000);
+  }
+}
+const myClock = new DigitalClock("my clock:");
+myClock.start();
+// a) Create a new class PrecisionClock that inherits from DigitalClock and adds the
+// parameter precision – the number of ms between 'ticks'. This precision parameter
+// should default to 1 second if not supplied.
+
+// b) Create a new class AlarmClock that inherits from DigitalClock and adds the
+// parameter wakeupTime in the format hh:mm. When the clock reaches this time, it
+// should print a 'Wake Up' message and stop ticking. This wakeupTime parameter should
+// default to 07:00 if not supplied.
+
+// a)
+class PrecisionClock extends DigitalClock {
+  constructor(prefix, precision = 1000) {
+    super(prefix);
+    this.precision = precision;
+  }
+
+  start() {
+    this.display();
+    this.timer = setInterval(() => this.display(), this.precision);
+  }
+}
+const myPrecisionClock = new PrecisionClock("my precision clock:", 500);
+myPrecisionClock.start();
+
+// b)
+class AlarmClock extends DigitalClock {
+  constructor(prefix, wakeupTime = "07:00") {
+    super(prefix);
+    this.wakeupTime = wakeupTime;
+  }
+
+  display() {
+    let date = new Date();
+    //create 3 variables in one go using array destructuring
+    let [hours, mins, secs] = [
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+    ];
+
+    hours = hours.toString().padStart(2, "0");
+    mins = mins.toString().padStart(2, "0");
+    secs = secs.toString().padStart(2, "0");
+
+    console.log(`${this.prefix} ${hours}:${mins}:${secs}`);
+
+    let [wakeUpHours, wakeUpMins] = this.wakeupTime.split(":");
+    if (hours === wakeUpHours && mins === wakeUpMins) {
+      console.log("Wake Up");
+      this.stop();
+    }
+  }
+}
+const myAlarmClock = new AlarmClock("my alarm clock:", "14:35");
+myAlarmClock.start();
