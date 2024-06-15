b702febd8cc18a35a4191530173282bd4d6942ad
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-15 15:18:25 +0800

    improve calculator library

--- module_5/calculator_app/library/calculatorLibrary.js
+++ module_5/calculator_app/library/calculatorLibrary.js
@@ -1,36 +1,29 @@
 const ClassWithId = require("./classWithId");
 const Logger = require("./loggerLibrary");
+const logger = new Logger();
 
 class Calculator extends ClassWithId {
-  #logger;
-
-  constructor() {
-    super();
-
-    this.#logger = new Logger();
-  }
-
   add = (num1, num2) => {
     const value = num1 + num2;
-    this.#logger.log(this, value);
+    logger.log(this, value);
     return value;
   };
 
   subtract = (num1, num2) => {
     const value = num1 - num2;
-    this.#logger.log(this, value);
+    logger.log(this, value);
     return value;
   };
 
   divide = (num1, num2) => {
     const value = num1 / num2;
-    this.#logger.log(this, value);
+    logger.log(this, value);
     return value;
   };
 
   multiply = (num1, num2) => {
     const value = num1 * num2;
-    this.#logger.log(this, value);
+    logger.log(this, value);
     return value;
   };
 }
