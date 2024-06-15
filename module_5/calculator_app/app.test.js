6bbfae12badd6178a301e397b6b0273307619b36
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-15 10:37:42 +0800

    rename and move test file

--- module_5/calculator_app/app.test.js
+++ module_5/calculator_app/app.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require("./app");
 
 describe("Calculator Routes", () => {
   // generate some random numbers to test the calculator
