a161bfc5c7230ed2bd148d24ac2e07304d025259
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-15 12:47:09 +0800

    module 4 ecommerce optional extras

--- module_4/ecommerce_website/index.js
+++ module_4/ecommerce_website/index.js
@@ -1,3 +1,5 @@
+// Api -----
+
 async function getProducts() {
   const res = await fetch(`https://fakestoreapi.com/products`);
   return res.json();
@@ -8,12 +10,8 @@ async function getCategories() {
   return res.json();
 }
 
-async function getCategoryProducts(category) {
-  const res = await fetch(
-    `https://fakestoreapi.com/products/category/${category}`
-  );
-  return res.json();
-}
+let products = [];
+let currentProducts = [];
 
 // Categories dropdown -----
 const categoriesDropdown = document.getElementById("category");
@@ -31,36 +29,75 @@ function initDropdown() {
 
 initDropdown();
 
-categoriesDropdown.addEventListener("change", (e) => refreshProducts());
+categoriesDropdown.addEventListener("change", () => applyFilters());
+
+// Sort dropdown -----
+const sortDropdown = document.getElementById("sort");
+sortDropdown.addEventListener("change", () => applyFilters());
+
+// Search -----
+const searchBar = document.getElementById("search");
+searchBar.addEventListener("input", () => applyFilters());
 
 // Products list -----
 const productsList = document.getElementById("products");
 
-async function refreshProducts() {
-  productsList.innerHTML = "Loading...";
-
-  let products = [];
-  if (categoriesDropdown.value) {
-    products = await getCategoryProducts(categoriesDropdown.value);
-  } else {
+async function applyFilters() {
+  // Fetch products if none exist
+  if (products.length == 0) {
+    productsList.innerHTML = "Loading...";
     products = await getProducts();
   }
 
+  // Clear html
   productsList.innerHTML = "";
 
-  products.forEach((product) => {
+  // Get products
+  currentProducts = [...products];
+
+  // Apply category filter
+  if (categoriesDropdown.value) {
+    currentProducts = currentProducts.filter(
+      (p) => p.category == categoriesDropdown.value
+    );
+  }
+
+  // Apply search filter
+  if (searchBar.value) {
+    const searchString = searchBar.value.toLowerCase();
+    currentProducts = currentProducts.filter((p) =>
+      p.title.toLowerCase().includes(searchString)
+    );
+  }
+
+  // Apply sort filter
+  if (sortDropdown.value) {
+    currentProducts = currentProducts.sort((p1, p2) => {
+      switch (sortDropdown.value) {
+        case "price-desc":
+          return p1.price - p2.price;
+        case "price-asc":
+          return p2.price - p1.price;
+        case "title":
+          return p1.title > p2.title ? 1 : -1;
+      }
+    });
+  }
+
+  currentProducts.forEach((product) => {
     const card = createProductCard(product);
     productsList.append(card);
   });
 }
 
-refreshProducts();
+applyFilters();
 
 // Template -----
 const template = document.getElementsByTagName("template")[0];
 
 function createProductCard(product) {
   const card = template.content.cloneNode(true);
+  card.querySelector(".category").textContent = product.category;
   card.querySelector(".image").src = product.image;
   card.querySelector(".title").textContent = product.title;
   card.querySelector(".price").textContent = `$${product.price}`;
