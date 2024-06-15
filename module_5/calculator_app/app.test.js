0388e3a49d2a8fd337c5e43a8ab1411a0aa17bdd
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-15 13:25:36 +0800

    refactor module 5 calc app tests

--- module_5/calculator_app/app.test.js
+++ module_5/calculator_app/app.test.js
@@ -1,56 +1,27 @@
 const request = require("supertest");
 const app = require("./app");
 
+// generate some random numbers to test the calculator
+let number1 = Math.floor(Math.random() * 1_000_000);
+let number2 = Math.floor(Math.random() * 1_000_000) + 1;
+
+const tests = [
+  { pathSegment: "add", expected: { result: number1 + number2 } },
+  { pathSegment: "subtract", expected: { result: number1 - number2 } },
+  { pathSegment: "divide", expected: { result: number1 / number2 } },
+  { pathSegment: "multiply", expected: { result: number1 * number2 } },
+];
+
 describe("Calculator Routes", () => {
-  // generate some random numbers to test the calculator
-  let number1 = Math.floor(Math.random() * 1_000_000);
-  let number2 = Math.floor(Math.random() * 1_000_000) + 1;
-
-  test("GET /calculator/add => sum of numbers", () => {
-    return request(app)
-      .get(`/calculator/add?num1=${number1}&num2=${number2}`)
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((response) => {
-        expect(response.body).toEqual({
-          result: number1 + number2,
+  tests.forEach((test) => {
+    it(`GET /calculator/${test.pathSegment}`, () => {
+      return request(app)
+        .get(`/calculator/${test.pathSegment}?num1=${number1}&num2=${number2}`)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(test.expected);
         });
-      });
-  });
-
-  test("GET /calculator/subtract => subtraction of numbers", () => {
-    return request(app)
-      .get(`/calculator/subtract?num1=${number1}&num2=${number2}`)
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((response) => {
-        expect(response.body).toEqual({
-          result: number1 - number2,
-        });
-      });
-  });
-
-  test("GET /calculator/divide => division of numbers", () => {
-    return request(app)
-      .get(`/calculator/divide?num1=${number1}&num2=${number2}`)
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((response) => {
-        expect(response.body).toEqual({
-          result: number1 / number2,
-        });
-      });
-  });
-
-  test("GET /calculator/multiply => multiplication of numbers", () => {
-    return request(app)
-      .get(`/calculator/multiply?num1=${number1}&num2=${number2}`)
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .then((response) => {
-        expect(response.body).toEqual({
-          result: number1 * number2,
-        });
-      });
+    });
   });
 });
