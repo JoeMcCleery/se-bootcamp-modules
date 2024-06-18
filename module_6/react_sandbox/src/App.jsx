718b6d232f1bd73c7f6701b4c4324fa96e7abdf7
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-18 20:20:52 +0800

    handle click test

--- module_6/react_sandbox/src/App.jsx
+++ module_6/react_sandbox/src/App.jsx
@@ -5,6 +5,12 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("Impostor");
+
+  function handleClick() {
+    setName("Joseph");
+    setCount(count + 1);
+  }
 
   return (
     <>
@@ -30,11 +36,9 @@ function App() {
           />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{name}&apos;s Page</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={handleClick}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
