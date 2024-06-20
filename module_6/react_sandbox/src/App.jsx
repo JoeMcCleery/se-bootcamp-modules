ed485f06cb567a8616caa5a3070b98e7a1f835c3
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-20 18:55:44 +0800

    Clear App.jsx

--- module_6/react_sandbox/src/App.jsx
+++ module_6/react_sandbox/src/App.jsx
@@ -1,49 +1,10 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Counter from "./components/Counter";
-import HoCLabel from "./components/HoCLabel";
 
-function App() {
+const App = () => {
   return (
     <>
-      <div>
-        <a
-          href="https://vitejs.dev"
-          target="_blank"
-        >
-          <img
-            src={viteLogo}
-            className="logo"
-            alt="Vite logo"
-          />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-        >
-          <img
-            src={reactLogo}
-            className="logo react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Joseph&apos;s React Page</h1>
-      <div className="card">
-        <HoCLabel label="Buttons Bellow:">
-          <Counter label="first" />
-          <Counter label="second" />
-        </HoCLabel>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="our-first-container">test</div>
     </>
   );
-}
-
+};
 export default App;
