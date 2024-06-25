ce7556895b299eb7c43163a69e9cce861e1d6c54
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-25 19:09:55 +0800

    module 6 exercise 4 completed

--- module_6/react_sandbox/src/components/Emoji.jsx
+++ module_6/react_sandbox/src/components/Emoji.jsx
@@ -5,11 +5,11 @@ const emojis = ["😀", "😄", "😅"];
 export default function Emoji() {
   const [index, setIndex] = useState(0);
   return (
-    <>
+    <div>
       <div>{emojis[index]}</div>
       <button onClick={() => setIndex((index + 1) % emojis.length)}>
         Change Mood
       </button>
-    </>
+    </div>
   );
 }
