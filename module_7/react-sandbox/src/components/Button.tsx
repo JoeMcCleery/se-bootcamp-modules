403234a47898f89582eb9927953d56fea4cda745
JoeMcCleery <mccleery.joseph@gmail.com>
2024-07-09 20:19:12 +0800

    Update button styling

--- module_7/react-sandbox/src/components/Button.tsx
+++ module_7/react-sandbox/src/components/Button.tsx
@@ -8,7 +8,7 @@ interface IButtonProps {
 export default function Button({ label, onClick }: IButtonProps) {
   return (
     <button
-      className="bg-slate-600 px-4 py-1 rounded-full hover:bg-slate-700 transition-colors ring-1 ring-slate-800"
+      className="bg-slate-700 px-4 py-1 rounded-full hover:bg-slate-800 transition-all ring-1 ring-slate-400 hover:ring-slate-500"
       onClick={onClick}
     >
       {label}
