cbd266d5707bd6413e88dedccf541a37fef59e19
JoeMcCleery <mccleery.joseph@gmail.com>
2024-07-09 21:12:40 +0800

    :S

--- module_7/react-sandbox/src/App.tsx
+++ module_7/react-sandbox/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ReactNode, useState } from "react";
+import "./App.css";
+import Clock from "./components/Clock";
+import Button from "./components/Button";
+import BitcoinRates from "./components/BitcoinRates";
+import BitcoinRatesCustom from "./components/BitcoinRatesCustom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface ILabData {
+  label: string;
+  content: ReactNode;
 }
 
-export default App
+const labContent: ILabData[] = [
+  { label: "Lab 1", content: <BitcoinRates /> },
+  { label: "Lab 2", content: <BitcoinRatesCustom /> },
+];
+
+function App() {
+  const [index, setIndex] = useState(-1);
+
+  const content =
+    index > -1 ? labContent[index].content : <em>Select a lab above</em>;
+
+  return (
+    <div className="p-4 grid gap-4 max-w-3xl m-auto">
+      <Clock />
+      <div className="flex flex-wrap gap-2 justify-center items-center bg-zinc-800 p-2 rounded-full">
+        {labContent.map((data, i) => (
+          <Button
+            key={i}
+            label={data.label}
+            onClick={() => setIndex(i)}
+          />
+        ))}
+      </div>
+
+      <div className="justify-self-center">{content}</div>
+    </div>
+  );
+}
+
+export default App;
