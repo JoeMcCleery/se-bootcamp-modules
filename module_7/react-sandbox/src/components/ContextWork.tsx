7e88309c9a467fe9357c12b206fa69676d47c42b
JoeMcCleery <mccleery.joseph@gmail.com>
2024-07-23 19:41:28 +0800

    Reducer context :O

--- module_7/react-sandbox/src/components/ContextWork.tsx
+++ module_7/react-sandbox/src/components/ContextWork.tsx
@@ -1,15 +1,28 @@
 import { useUserContext } from "./UserProvider";
 import Button from "./Button";
+import { useState } from "react";
 
 export default function ContextWork() {
-  const { currentUser, setCurrentUser } = useUserContext();
+  const { state, updateUser, toggleMode } = useUserContext();
+  const [username, setUsername] = useState(state.user.name);
 
   return (
     <div>
-      <p>{currentUser.name}</p>
+      <p>{state.user.name}</p>
+      <p>{state.mode}</p>
+      <input
+        title="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="text-black"
+      />
       <Button
         label="Change User"
-        onClick={() => setCurrentUser({ name: "Bob" })}
+        onClick={() => updateUser({ name: username })}
+      />
+      <Button
+        label="Toggle Mode"
+        onClick={toggleMode}
       />
     </div>
   );
--- module_7/react-sandbox/src/components/UserProvider.tsx
+++ module_7/react-sandbox/src/components/UserProvider.tsx
@@ -1,31 +1,68 @@
 import {
   createContext,
-  Dispatch,
   PropsWithChildren,
-  SetStateAction,
   useContext,
-  useState,
+  useReducer,
 } from "react";
 
 interface IUser {
   name: string;
 }
 
-interface IUserContext {
-  currentUser: IUser;
-  setCurrentUser: Dispatch<SetStateAction<IUser>>;
+interface IUserContextState {
+  user: IUser;
+  mode: "light" | "dark";
 }
 
-const UserContext = createContext<IUserContext>({
-  currentUser: { name: "" },
-  setCurrentUser: () => {},
-});
+interface IUserContext {
+  state: IUserContextState;
+  updateUser: (user: IUser) => void;
+  toggleMode: () => void;
+}
+
+const defaultState: IUserContextState = {
+  user: { name: "Jeff" },
+  mode: "light",
+};
+
+const defaultContext: IUserContext = {
+  state: defaultState,
+  updateUser: () => {},
+  toggleMode: () => {},
+};
+
+const UserContext = createContext<IUserContext>(defaultContext);
+
+interface IUserAction {
+  type: "UPDATE_USER" | "TOGGLE_MODE";
+  payload?: IUser;
+}
+
+const userReducer = (
+  state: IUserContextState,
+  action: IUserAction
+): IUserContextState => {
+  switch (action.type) {
+    case "UPDATE_USER":
+      return { ...state, user: action.payload! };
+    case "TOGGLE_MODE":
+      return { ...state, mode: state.mode == "light" ? "dark" : "light" };
+  }
+};
 
 export const UserProvider = ({ children }: PropsWithChildren) => {
-  const [currentUser, setCurrentUser] = useState<IUser>({ name: "Jeff" });
+  const [state, dispatch] = useReducer(userReducer, defaultState);
+
+  function updateUser(user: IUser) {
+    dispatch({ type: "UPDATE_USER", payload: user });
+  }
+
+  function toggleMode() {
+    dispatch({ type: "TOGGLE_MODE" });
+  }
 
   return (
-    <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+    <UserContext.Provider value={{ state, updateUser, toggleMode }}>
       {children}
     </UserContext.Provider>
   );
