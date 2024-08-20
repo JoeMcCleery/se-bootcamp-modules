f23f2a690f5cec469ad7e264235e8ee04bc16945
JoeMcCleery <mccleery.joseph@gmail.com>
2024-08-20 20:42:00 +0800

    add post model + user relations

--- module_9/express-mysql/models/index.js
+++ module_9/express-mysql/models/index.js
@@ -1,15 +1,22 @@
 "use strict";
 
-const User = require("./user"); //require the model
+// Models
+const User = require("./user");
+const Post = require("./post");
 
+// Sync to database
 async function init() {
-  await User.sync(); // sync the model
-  // also sync any extra models here
+  await User.sync();
+  await Post.sync();
 }
 
 init();
 
+// Relations
+Post.belongsTo(User);
+User.hasMany(Post);
+
 module.exports = {
-  User, // export the model
-  // also export any extra models here
+  User,
+  Post,
 };
