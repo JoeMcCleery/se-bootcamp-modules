f745f041fd5ad9b5abfc076f3b47282f48133c56
JoeMcCleery <mccleery.joseph@gmail.com>
2024-08-20 20:48:30 +0800

    Add comment model and its associations

--- module_9/express-mysql/models/index.js
+++ module_9/express-mysql/models/index.js
@@ -3,18 +3,25 @@
 // Models
 const User = require("./user");
 const Post = require("./post");
+const Comment = require("./comment");
+
+// Relations
+Post.belongsTo(User);
+User.hasMany(Post);
+
+Comment.belongsTo(User);
+User.hasMany(Comment);
+Comment.belongsTo(Post);
+Post.hasMany(Comment);
 
 // Sync to database
 async function init() {
   await User.sync();
   await Post.sync();
+  await Comment.sync();
 }
 
 init();
-
-// Relations
-Post.belongsTo(User);
-User.hasMany(Post);
 
 module.exports = {
   User,
