94a2eb383907bbdaa2abc3e09c87f97aa76f2939
JoeMcCleery <mccleery.joseph@gmail.com>
2024-08-22 20:47:41 +0800

    basic functionality

--- module_9/express-sockets/index.js
+++ module_9/express-sockets/index.js
@@ -10,8 +10,22 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// Connect
 io.on("connection", (socket) => {
+  console.log("a user connected");
   io.emit("connection", "a user connected");
+
+  // Chat message
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  // Disconnect
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    io.emit("connection", "user disconnected");
+  });
 });
 
 server.listen(3000, () => {
