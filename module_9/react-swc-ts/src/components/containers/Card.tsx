e021f556fd93181e82d4da75801c140c1c8ecda4
JoeMcCleery <mccleery.joseph@gmail.com>
2024-08-24 14:17:37 +0800

    Card styling

--- module_9/react-swc-ts/src/components/containers/Card.tsx
+++ module_9/react-swc-ts/src/components/containers/Card.tsx
@@ -6,7 +6,7 @@ interface ICardProps extends PropsWithChildren {
 
 export default function Card({ title, children }: ICardProps) {
   return (
-    <div className="bg-slate-200 rounded shadow border border-slate-300 divide-y divide-slate-300">
+    <div className="bg-slate-300 rounded shadow border border-slate-400 divide-y divide-slate-400">
       {title && (
         <div className="p-4">
           <p className="text-slate-600">{title}</p>
