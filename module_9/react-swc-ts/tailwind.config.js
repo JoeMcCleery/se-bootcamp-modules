4090d3c58b2d48840f2dc43ae413969781e64f03
JoeMcCleery <mccleery.joseph@gmail.com>
2024-08-25 15:06:43 +0800

    Add roboto font

--- module_9/react-swc-ts/tailwind.config.js
+++ module_9/react-swc-ts/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,svg}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: [
+          "Roboto",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
   },
   plugins: [],
 };
